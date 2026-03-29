ed802c07e238ca1f785531e124cbc0cede01ada4
Aryan Saini <09aryan.lucky@gmail.com>
2026-03-29 20:06:53 +0530

    resolved routing error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import CreatePost from "./pages/CreatePost";
 import Feed from "./pages/Feed";
@@ -9,6 +13,9 @@ const App = () => {
     <>
       <Router>
         <Routes>
+          {/* When user hits "/", it automatically redirects to "/create-post" */}
+          {/* "replace" prevents extra history entry (better UX)  */}
+          <Route path="/" element={<Navigate to="/create-post" replace />} />
           <Route path="/create-post" element={<CreatePost />} />
           <Route path="/feed" element={<Feed />} />
         </Routes>
